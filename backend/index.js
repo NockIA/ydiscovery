889d55ed6ad433712c68033fa02495fc4622ccae
ckevin <kevin.chaffaux@ynov.com>
2023-11-15 14:26:26 +0100

    :sparkles: | add cors

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import mysql from 'mysql';
+import cors from "cors";
 
 const app = express();
-app.use(cors());
+
+app.use(cors);
 app.use(express.json());
 
-const db = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password: "Nockia2004",
-    database : "database"
-})
+// const db = mysql.createConnection({
+//     host : "localhost",
+//     user : "root",
+//     password: "Nockia2004",
+//     database : "database"
+// })
 
 const port = 8080;
 
