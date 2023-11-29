42b2226da9a012e7353a457d636e2776bb7cf4af
ckevin <kevin.chaffaux@ynov.com>
2023-11-29 15:26:46 +0100

    :sparkles: | add link between database and back

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,50 @@
-import express from "express";
-import mysql from 'mysql';
-import cors from "cors";
-
+const express = require("express")
+const cors = require('cors');
 const app = express();
-
-app.use(cors);
+const connection = require('./database')
 app.use(express.json());
+app.use(cors());
 
-// const db = mysql.createConnection({
-//     host : "localhost",
-//     user : "root",
-//     password: "Nockia2004",
-//     database : "database"
-// })
-
-const port = 8080;
-
-
-app.get('/',(req,res) => {
-    res.json("hello world");
-})
+const port = 3000;
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port} at http://localhost:8080/`);
-
+    console.log(`Server started on port ${port} at http://localhost:${port}/`);
 })
 
+app.get('/', (req, res) => {
+    res.send('Hey there')
+})
+
+app.get('/authors', (req, res) => {
+    connection.connect((err) => {
+        if (err) {
+            console.error("Connexion error : " + err.stack);
+            return;
+        }
+        console.log("Connected to database");
+    })
+    const query = "SELECT CONCAT(firstname ,' ', lastname) as name , DATE_FORMAT(birthdate, '%d/%m/%Y') AS birthdate FROM authors";
+    connection.query(query, (err, rows) => {
+        if (err) throw err;
+        res.json(rows);
+    })
+    connection.end;
+    console.log('Database closed');
+})
+
+app.get('/articles', (req, res) => {
+    connection.connect((err) => {
+        if (err) {
+            console.error("Connexion error : " + err.stack);
+            return;
+        }
+        console.log("Connected to database");
+    })
+    const query = "SELECT CONCAT(firstname ,' ',lastname) AS authorName, title , subtitle , content , illustrationLink , DATE_FORMAT(publicationDate , '%d/%m/%Y') AS publicationDate FROM articles JOIN authors ON authors.authorId = articles.authorId;"
+    connection.query(query, (err, rows) => {
+        if (err) throw err;
+        res.json(rows);
+    })
+    console.log('Database closed');
+    connection.end;
+})
