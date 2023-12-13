212181305daade80d4cf9d6dcf769c318f2fd925
ckevin <kevin.chaffaux@ynov.com>
2023-12-13 11:17:15 +0100

    :sparkles: | add login/register methods

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,26 @@
 const express = require("express")
 const cors = require('cors');
 const app = express();
-const connection = require('./database')
+const connection = require('./database');
+const generatePasswd = require('generate-password');
+const crypto = require('crypto');
+const { log } = require("console");
 app.use(express.json());
 app.use(cors());
 
 const port = 3000;
+
+const generatePassword = (charLen) => {
+    return generatePasswd.generate({
+        length: charLen,
+        numbers: true
+    })
+}
+
+const hashPassword = (algorithm, base, passwd) => {
+    return crypto.createHash(algorithm).update(passwd).digest(base);
+}
+
 
 app.listen(port, () => {
     console.log(`Server started on port ${port} at http://localhost:${port}/`);
@@ -47,4 +62,41 @@ app.get('/articles', (req, res) => {
     })
     console.log('Database closed');
     connection.end;
+})
+
+app.post('/doesuserexist', (req, res) => {
+    const emp = req.body;
+    connection.query('SELECT * FROM accounts WHERE email = ? AND password = ?;', [emp.email, hashPassword('sha256', 'base64', emp.password)], (err, result) => {
+        if (err) {
+            res.json({ err });
+            return;
+        }
+        if (result.length > 0) {
+            res.json({ exist: true });
+            return;
+        }
+        return res.json({ exist: false });
+    });
+})
+
+app.post('/signup', (req, res) => {
+    const emp = req.body;
+    connection.query('SELECT * FROM accounts WHERE email = ?', [emp.email], (err, result) => {
+        if (err) {
+            res.json({ err });
+            return;
+        }
+        if (result.length > 0) {
+            res.json({ exist: true });
+            return;
+        } else {
+            connection.query('INSERT INTO accounts (email , password) VALUES (? , ?);', [emp.email, hashPassword('sha256', 'base64', emp.password)], (err, result) => {
+                if (err) {
+                    res.json({ err });
+                    return;
+                }
+                res.json({ status: true })
+            })
+        }
+    })
 })
