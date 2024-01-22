75954a32f7f08e6309e6f65ad94eb7e43a7e5d40
benphihe <150243312+benphihe@users.noreply.github.com>
2024-01-22 08:02:54 +0100

    modification des pages signIn et pages signUp

--- client/src/pages/templates/signIn.jsx
+++ client/src/pages/templates/signIn.jsx
@@ -1,10 +1,28 @@
 import '../styles/signin.css'
 
 export function SignIn() {
-
+  
   return (
     <>
       <div>
+      <div className='container'>
+          <form action="" method="post">
+            <div>
+              <label htmlFor="">Email</label>
+              <input type="email" className="form-control"/>
+            </div>
+            <div>
+              <label htmlFor="">mot de passe </label>
+              <input type="password" className="form-control"/>
+            </div>
+            <div>
+              <button>
+                s'identifier
+              </button>
+            </div> 
+          </form>
+
+        </div>
           <h1>SignIn</h1>
       </div>
     </>
--- client/src/pages/templates/signUp.jsx
+++ client/src/pages/templates/signUp.jsx
@@ -1,11 +1,38 @@
 import '../styles/signup.css'
+import React,{useState} from 'react'
 
 export function SingUp() {
+  const [Email, setEmail] = useState('')
+  const [Prenom, setPrenom] = useState('')
+  const [Nom, setNom] = useState('')
+  const [password ,setPassword] = useState('')
 
   return (
     <>
       <div>
-          <h1>SignIn</h1>
+        <div className='container'>
+            <div>
+              <label htmlFor="">Email</label>
+              <input onChange={(e)=>setEmail(e.target.value)} type="email" />
+            </div>
+            <div>
+              <label htmlFor="">Prenom</label>
+              <input onChange={(e)=>setPrenom(e.target.value)} type="texte" />
+            </div>
+            <div>
+              <label htmlFor="">Nom</label>
+              <input onChange={(e)=>setNom(e.target.value)} type="texte" />
+            </div>
+            <div>
+              <label htmlFor="">password </label>
+              <input onChange={(e)=>setPassword(e.target.value)} type="password" />
+            </div>
+            <div>
+              <button>
+                continuer
+              </button>
+            </div> 
+        </div>
       </div>
     </>
   )
