4f275ef2e8d4457ae54db6c87f7ff8eb09794b99
ckevin <kevin.chaffaux@ynov.com>
2024-01-23 09:30:17 +0100

    :card_file_box: | edit db tables

--- creation_database/createDbDatas.sql
+++ creation_database/createDbDatas.sql
@@ -6,12 +6,9 @@ USE `ydiscovery_datas`;
 
 CREATE TABLE `authors` (
     `authorId` INTEGER NOT NULL AUTO_INCREMENT,
-    `firstname` VARCHAR(20) NOT NULL,
-    `lastname` VARCHAR(20) NOT NULL,
-    `birthDate` DATE NOT NULL,
+    `authorName` VARCHAR(50),
     PRIMARY KEY(`authorId`)
 );
-
 
 CREATE TABLE `articles` (
     `articleId` INTEGER NOT NULL AUTO_INCREMENT,
@@ -23,4 +20,16 @@ CREATE TABLE `articles` (
     `content` TEXT NOT NULL,
     FOREIGN KEY (`authorId`) REFERENCES authors(`authorId`),
     PRIMARY KEY (`articleId`)
+);
+
+CREATE TABLE `accounts` (
+    `accountId` INTEGER NOT NULL AUTO_INCREMENT,
+    `authorId` INTEGER NOT NULL,
+    `firstname` VARCHAR(50) NOT NULL,
+    `lastname` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(50) NOT NULL,
+    `isAdmin` BOOLEAN NOT NULL,
+    PRIMARY KEY(`accountId`),
+    FOREIGN KEY (`authorId`) REFERENCES authors(`authorId`)
 );
