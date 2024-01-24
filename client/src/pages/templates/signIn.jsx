cb80df7a2ac66266ab7240a9ffc67c557e590902
benphihe <150243312+benphihe@users.noreply.github.com>
2024-01-24 09:33:48 +0100

    creation du css pour la pages signin

--- client/src/pages/templates/signIn.jsx
+++ client/src/pages/templates/signIn.jsx
@@ -1,29 +1,26 @@
 import '../styles/signin.css'
-
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 export function SignIn() {
-  
+
   return (
     <>
-      <div>
       <div className='container'>
-          <form action="" method="post">
-            <div>
-              <label htmlFor="">Email</label>
-              <input type="email" className="form-control"/>
-            </div>
-            <div>
-              <label htmlFor="">mot de passe </label>
-              <input type="password" className="form-control"/>
-            </div>
-            <div>
-              <button>
-                s'identifier
-              </button>
-            </div> 
-          </form>
-
+        <div>
+          <h1>Sign In</h1>
+          <h5>Hello and welcome ! We are delighted to see you again. Please enter your credentials below to access your account.</h5>
         </div>
-          <h1>SignIn</h1>
+        <div className='container_inputs' >
+          <input placeholder='Email' type="email" className="form-control" />
+          <input placeholder='Password' type="password" className="form-control" />
+          <button>
+            s'identifier
+          </button>
+        </div>
+        <div className='container-account'>
+        <h5>You do not have an account ?</h5>
+        <Link to={'/signup'}>create a account</Link>
+        </div>
       </div>
     </>
   )
